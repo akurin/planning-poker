ae0cebc13fd3ea07f06de5aeea165a241295aa2f
Alexey Kurin <alexey.kurin.nik@gmail.com>
2020-11-12 01:21:40 +0300

    wip

--- bdd/features/step_definitions/pages/startGamePage.ts
+++ bdd/features/step_definitions/pages/startGamePage.ts
@@ -8,6 +8,8 @@ export class StartGamePage {
   }
 
   async setGameName(gameName: string): Promise<void> {
+    const s = await (await this._driver).takeScreenshot();
+    console.log(s);
     const gameNameInput = await this._driver.findElement(
       By.css("[data-qa='game-name']")
     );
