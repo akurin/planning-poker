950571c36d4a0096e28d8944725a6a1e060da472
Alexey Kurin <alexey.kurin.nik@gmail.com>
2020-11-30 22:29:20 +0300

    Add Home page

---
+++ ui/src/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Link from "svelte-routing/src/Link.svelte";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        const res = await fetch(`http://localhost:5000/api/`, {
+            method: "POST",
+            body: JSON.stringify({
+                
+            }),
+        });
+        const photos = await res.json();
+        console.log(photos);
+    });
+</script>
+
+<main>
+    <p>New game</p>
+    <Link to="games/123"><input type="button" data-qa="start-game" /></Link>
+    <input type="text" data-qa="game-name" />
+</main>
